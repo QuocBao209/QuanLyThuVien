9fa315b64cd4d3f72f060e4dc48a2a524deea73d
QuocBao209 <baobao2092004@gmail.com>
2025-02-17 17:51:38 +0700

    Thiết kế trang home và trang bookFilter

--- LibraryManagement/src/main/java/com/project/demo/controller/HomeController.java
+++ LibraryManagement/src/main/java/com/project/demo/controller/HomeController.java
@@ -2,12 +2,20 @@ package com.project.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-	@GetMapping("/home") 
+	@GetMapping("") 
 	public ModelAndView showHomePage() {
 		return new ModelAndView("home");
+	}
+	
+	@PostMapping("/book-filter")
+	public ModelAndView bookFilterPage() {
+		return new ModelAndView("bookFilter");
 	}
 }
